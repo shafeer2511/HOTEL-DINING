344710c1f1e2d0229d14a52802adb5e49c2ee8af
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2024-09-04 22:00:36 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/Login';
 import RegisterPage from './components/Register';
+import HomePage from './components/HomePage';
+
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
