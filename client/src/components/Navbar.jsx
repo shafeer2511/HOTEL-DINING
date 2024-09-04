e3e7ecad7dcc17ef3f7fd114e7b5f5a7e91b292b
Vishal15276 <126046094+Vishal15276@users.noreply.github.com>
2024-09-04 16:31:04 +0530

    react

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/navbar.css';
+import '../styles/Navbar.css';
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'; // Import the arrow icon
 
