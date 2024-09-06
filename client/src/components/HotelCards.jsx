c0ecf4055f1bb189b4d209cefb02656e6c863025
THAMILVASANTH <126443048+THAMILVASANTH@users.noreply.github.com>
2024-09-06 13:29:23 +0530

    enna

--- client/src/components/HotelCards.jsx
+++ client/src/components/HotelCards.jsx
@@ -3,6 +3,7 @@ import '../styles/hotelCards.css';
 
 function HotelCards({ hotel }) {
   return (
+    
     <div className="hotel-card">
       <img src={hotel.image} alt={hotel.name} className="hotel-card-image" />
       <div className="hotel-card-content">
