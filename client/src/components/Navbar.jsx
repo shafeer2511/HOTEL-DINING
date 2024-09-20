3b43035aadd81ddc58b0c4c0b87b7037c9c644a7
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2024-09-20 14:08:57 +0530

    commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,80 +1,31 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/Navbar.css';
-import PermIdentityIcon from '@mui/icons-material/PermIdentity';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import React from 'react';
+import '../styles/navbar.css'; // Make sure the CSS file is updated
 
-function Navbar() {
-  const [dropdown, setDropdown] = useState(false);
-  const dropdownRef = useRef(null);
-
-  // Toggle dropdown visibility
-  const handleDropdownToggle = () => {
-    setDropdown(prev => !prev);
-  };
-
-  // Handle clicks outside of the dropdown
-  const handleClickOutside = (event) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setDropdown(false);
-    }
-  };
-
-  // Close dropdown when clicking on any link
-  const handleLinkClick = () => {
-    setDropdown(false);
-  };
-
-  useEffect(() => {
-    // Add event listener to detect clicks outside the dropdown
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      // Cleanup event listener
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
+const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
-          FREENCY DiNE-iN
-        </Link>
-        <ul className="navbar-menu">
-          <li className="navbar-profile">
-            <div className="profile-container" onClick={handleDropdownToggle}>
-              <PermIdentityIcon className="navbar-profile-icon" />
-              <ExpandMoreIcon className="navbar-expand-icon" />
-            </div>
-            {dropdown && (
-              <ul className="navbar-dropdown" ref={dropdownRef}>
-                <li className="navbar-dropdown-item">
-                  <Link to="/" className="navbar-dropdown-link" onClick={handleLinkClick}>
-                    Home
-                  </Link>
-                </li>
-                <li className="navbar-dropdown-item">
-                  <Link to="/Book Table" className="navbar-dropdown-link" onClick={handleLinkClick}>
-                    Dining
-                  </Link>
-                </li>
-                <li className="navbar-dropdown-item">
-                  <Link to="/login" className="navbar-dropdown-link" onClick={handleLinkClick}>
-                    Login
-                  </Link>
-                </li>
-                <li className="navbar-dropdown-item">
-                  <Link to="/register" className="navbar-dropdown-link" onClick={handleLinkClick}>
-                    Sign Up
-                  </Link>
-                </li>
-              </ul>
-            )}
-          </li>
-        </ul>
+        {/* Left section: Logo */}
+        <div className="logo">
+          <a href="#">FREENCY DINE-IN</a>
+        </div>
+
+        {/* Center section: Navigation Links */}
+        <div className="navbar-links">
+          <a href="#">Home</a>
+          <a href="#">Book a Table</a>
+          <a href="#">Menu</a>
+        </div>
+
+        {/* Right section: Login */}
+        <div className="login-section">
+          <a href="#">
+            LOGIN <span className="login-icon">👤</span>
+          </a>
+        </div>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
